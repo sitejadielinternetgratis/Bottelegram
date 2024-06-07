a9561941258ebbd2b9dea9d5b3a707ac27a00763
sitejadielinternetgratis <jadieldossantosribeiro@gmail.com>
2024-06-07 13:30:48 -0300

    Create deploy_bot.sh

---
+++ deploy_bot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download do código do bot
+wget -qO- https://raw.githubusercontent.com/seu_usuario/seu_repositorio/main/seu_bot.py > seu_bot.py
+
+# Instalação das dependências, se necessário
+# Comando para instalar as dependências...
+
+# Iniciar o bot
+python3 bot.py
